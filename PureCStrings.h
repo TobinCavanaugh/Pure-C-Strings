b71a89091ef02319bebff1cac760d29186c33bec
Jup1t3rTobinC <tobincavanaugh@gmail.com>
2023-05-01 17:57:25 -0700

    New header image, re-organized header file

--- PureCStrings.h
+++ PureCStrings.h
@@ -20,18 +20,21 @@ typedef enum StringFlags{
     SF_IGNORECASE = 1 << 0,   // bit 0
 } StringFlags;
 
-void *MallocLog(size_t size, const char *message);
-unsigned int PureString_Length(PureString *string);
-void PureString_Concatenate(PureString *ps, const char *addition, size_t length);
-void PureString_Copy(PureString *dest, PureString *source, unsigned int offset);
-unsigned int PureString_Contains(PureString *aString, const char *sub, StringFlags flag);
 PureString *PureString_Create(const char *initial);
 PureString *PureString_CreateEmpty(unsigned int length);
-void PureString_Trim(PureString *ps, unsigned int index);
-void PureString_RemoveRange(PureString *ps, unsigned int start, unsigned int end);
+
+void *MallocLog(size_t size, const char *message);
+
+void PureString_Concatenate(PureString *ps, const char *addition, size_t length);
+void PureString_Copy(PureString *dest, PureString *source, unsigned int offset);
 void PureString_Destroy(PureString * ps);
-void PureString_Substring(PureString *ps, unsigned int start, unsigned int end);
-void PureString_ReplaceChar(PureString *ps, char from, char to, StringFlags flags);
+void PureString_RemoveRange(PureString *ps, unsigned int start, unsigned int end);
 void PureString_Replace(PureString *ps, char *from, char *to, StringFlags sf);
+void PureString_ReplaceChar(PureString *ps, char from, char to, StringFlags flags);
+void PureString_Substring(PureString *ps, unsigned int start, unsigned int end);
 void PureString_ToLower(PureString *ps);
 void PureString_ToUpper(PureString *ps);
+void PureString_Trim(PureString *ps, unsigned int index);
+
+unsigned int PureString_Length(PureString *string);
+unsigned int PureString_Contains(PureString *aString, const char *sub, StringFlags flag);
